9fff6247d7fdefd9c04fbf7261c735fe5f9ad5a8
alondo11 <54036339+AlonDoron@users.noreply.github.com>
2023-03-16 15:07:05 +0200

    Added support for uploading images in base64

--- src/routes/addItemPage.tsx
+++ src/routes/addItemPage.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FormikHelpers, useFormik } from 'formik';
 import { Container } from '@mui/system';
 import * as yup from 'yup';
-import { Button, CircularProgress, TextField } from '@mui/material';
+import { Button, CircularProgress, TextField, Typography } from '@mui/material';
 
 type Props = {}
 
@@ -21,6 +21,8 @@ const validationSchema = yup.object({
 })
 
 const addItemPage = (props: Props) => {
+    const [images, setImages] = useState<string[]>();
+
     const formik = useFormik({
         initialValues: {
             title: '',
@@ -29,31 +31,57 @@ const addItemPage = (props: Props) => {
         validationSchema: validationSchema,
         onSubmit: (values: FormValues, { setSubmitting }: FormikHelpers<FormValues>) => {
             setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
+                alert(JSON.stringify({ values, images }, null, 2));
                 setSubmitting(false);
             }, 2000);
         },
     });
 
+    const toBase64 = (file: File): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => resolve(reader.result as string)
+            reader.onerror = error => reject(error);
+        });
+    };
+
+    const convertToBase64 = async (evt: React.ChangeEvent<HTMLInputElement>) => {
+        let filesPromises: Promise<string>[] = [];
+        debugger
+        if (evt.target.files && evt.target.files.length) {
+            Array.from(evt.target.files).forEach(file => filesPromises.push(toBase64(file)));
+            const filesInBase64 = await Promise.all(filesPromises);
+            setImages(filesInBase64);
+        }
+    }
+
     return (
         <Container>
+            <Typography variant='h3'>Add New Item</Typography>
             <form onSubmit={formik.handleSubmit}>
                 <TextField fullWidth
+                    required
                     id='title'
                     name='title'
-                    label="title"
+                    label="Title"
+                    margin='normal'
                     value={formik.values.title}
                     onChange={formik.handleChange}
                     error={formik.touched.title && Boolean(formik.errors.title)}
                     helperText={formik.touched.title && formik.errors.title} />
                 <TextField fullWidth
+                    required
                     id='description'
                     name='description'
-                    label="description"
+                    label="Description"
+                    multiline
+                    margin='normal'
                     value={formik.values.description}
                     onChange={formik.handleChange}
                     error={formik.touched.description && Boolean(formik.errors.description)}
                     helperText={formik.touched.description && formik.errors.description} />
+                <input type="file" onChange={convertToBase64} multiple required />
                 <Button variant='contained' type="submit" disabled={formik.isSubmitting}>
                     Submit
                 </Button>
