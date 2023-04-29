8866f5e474b2e9f8684c085e7ea37698023e1316
AlonDoron <alon1212112@gmail.com>
2023-04-29 23:14:06 +0300

    Forgot to commit locations mock

---
+++ src/mocks/locations.ts
@@ -0,0 +1,13 @@
+import { IMarkerDetails } from "../types";
+
+let locations: IMarkerDetails[] = [
+  { name: "Serviceberry", lat: 43.6535901061267, lng: -79.4579220211632 },
+  {
+    name: "Honey locust, 'Skyline'",
+    lat: 43.6751394806294,
+    lng: -79.3424120001337,
+  },
+  { name: "Oak, white", lat: 43.6731194268692, lng: -79.51786185076651 },
+];
+
+export default locations;
