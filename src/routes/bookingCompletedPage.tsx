34be200e8805354d1e78a03722fd1767f781cbd4
AlonDoron <alon1212112@gmail.com>
2023-05-15 21:03:46 +0300

    Removed HttpClient and updated usages of new API

--- src/routes/bookingCompletedPage.tsx
+++ src/routes/bookingCompletedPage.tsx
@@ -1,159 +1,185 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Container } from "@mui/system";
-import {
-    Button,
-    Typography,
-    Divider,
-    Card,
-    CardMedia,
-} from "@mui/material";
-import HttpClient from "../api/HttpClient";
+import { Button, Typography, Divider, Card, CardMedia } from "@mui/material";
 import { useLocation, useNavigate, useParams } from "react-router";
 import { IFullItemDetailsNew } from "../types";
 import DateRangePicker from "../components/DateRangePicker/DateRangePicker";
 import { formatImagesOnRecieve } from "../utils/imagesUtils";
+import { getItem } from "../api/ItemService";
 
 type IFullItemDetailsParams = {
-    itemId: string;
+  itemId: string;
 };
 
 type Props = {
-    startDate: Date;
-    endDate: Date;
-    onChange: (dates: any) => void;
-    datesToExclude: Date[];
+  startDate: Date;
+  endDate: Date;
+  onChange: (dates: any) => void;
+  datesToExclude: Date[];
 };
 
 interface IRowData {
-    key: string;
-    value: string;
+  key: string;
+  value: string;
 }
 
 interface ITableData {
-    tableData: IRowData[];
+  tableData: IRowData[];
 }
 
 const Row = ({ tableData }: ITableData) => {
-    return (
-        <div>
-            {tableData.map((row, i) => (
-                <div key={i}>
-                    <div style={{ display: "flex", flexDirection: "row" }}>
-                        <Typography
-                            variant="body1"
-                            sx={{ flexBasis: "50%", color: "darkgray" }}
-                        >
-                            {row.key}
-                        </Typography>
-                        <Typography variant="body1" sx={{ flexBasis: "50%" }}>
-                            {row.value}
-                        </Typography>
-                    </div>
-                    {i < tableData.length - 1 && <Divider sx={{ margin: "5px" }} />}
-                </div>
-            ))}
+  return (
+    <div>
+      {tableData.map((row, i) => (
+        <div key={i}>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <Typography
+              variant="body1"
+              sx={{ flexBasis: "50%", color: "darkgray" }}
+            >
+              {row.key}
+            </Typography>
+            <Typography variant="body1" sx={{ flexBasis: "50%" }}>
+              {row.value}
+            </Typography>
+          </div>
+          {i < tableData.length - 1 && <Divider sx={{ margin: "5px" }} />}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
-const BookingCompletedPage = ({ startDate, endDate, datesToExclude, onChange }: Props) => {
-    const { state } = useLocation();
-    const { selectedStartDate, selectedEndDate, excludedDates, onDateChange } = state;
+const BookingCompletedPage = ({
+  startDate,
+  endDate,
+  datesToExclude,
+  onChange,
+}: Props) => {
+  const { state } = useLocation();
+  const {
+    selectedStartDate,
+    selectedEndDate,
+    excludedDates,
+    onDateChange,
+  } = state;
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    let { itemId } = useParams<IFullItemDetailsParams>();
+  let { itemId } = useParams<IFullItemDetailsParams>();
 
-    const [itemDetails, setItemDetails] = useState<IFullItemDetailsNew>({
-        categories: [],
-        condition: "",
-        itemId: "",
-        title: "",
-        images: [],
-        description: "",
-        excludedDates: [],
-    });
+  const [itemDetails, setItemDetails] = useState<IFullItemDetailsNew>({
+    categories: [],
+    condition: "",
+    itemId: "",
+    title: "",
+    images: [],
+    description: "",
+    excludedDates: [],
+  });
 
-    const [serverRequestError, setServerRequestError] = useState<any>();
+  const [serverRequestError, setServerRequestError] = useState<any>();
 
-    useEffect(() => {
-        const getFullDetails = async () => {
-            let fullDetails: IFullItemDetailsNew;
-            try {
-                fullDetails = await HttpClient.get(`items/${itemId}`);
-                setItemDetails(fullDetails);
-            }
-            catch (err) {
-                console.log("Error while loading item");
-                setServerRequestError(err);
-                //todo:show error
-            }
+  useEffect(() => {
+    const getFullDetails = async () => {
+      let fullDetails: IFullItemDetailsNew;
+      if (itemId) {
+        try {
+          fullDetails = (await getItem(itemId)) as IFullItemDetailsNew;
+          setItemDetails(fullDetails);
+        } catch (err) {
+          console.log("Error while loading item");
+          setServerRequestError(err);
+          //todo:show error
         }
-        getFullDetails();
-    }, []);
+      }
+    };
+    getFullDetails();
+  }, []);
 
-    return (
-        <Container>
-            <Typography component={'span'} variant="h3">Booking Request is Completed</Typography>
-            <div style={{ display: "flex", alignItems: "flex-start", flexDirection: "row" }}>
-                <Card sx={{ marginBottom: "10px", width: "230px", marginRight: "10px" }}>
-                    {itemDetails.images && (
-                        <CardMedia
-                            component="img"
-                            style={{ height: "230px", width: "230px" }}
-                            image={formatImagesOnRecieve(itemDetails.images)[0]}
-                        >
-                        </CardMedia>
-                    )}
-                </Card>
+  return (
+    <Container>
+      <Typography component={"span"} variant="h3">
+        Booking Request is Completed
+      </Typography>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "flex-start",
+          flexDirection: "row",
+        }}
+      >
+        <Card
+          sx={{ marginBottom: "10px", width: "230px", marginRight: "10px" }}
+        >
+          {itemDetails.images && (
+            <CardMedia
+              component="img"
+              style={{ height: "230px", width: "230px" }}
+              image={formatImagesOnRecieve(itemDetails.images)[0]}
+            ></CardMedia>
+          )}
+        </Card>
 
-                <div style={{ display: "flex", flexDirection: "column" }}>
-                    <Typography variant="h5">{itemDetails.title}</Typography>
-                    <Typography variant="body1">{itemDetails.description}</Typography>
-                </div>
-            </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <Typography variant="h5">{itemDetails.title}</Typography>
+          <Typography variant="body1">{itemDetails.description}</Typography>
+        </div>
+      </div>
 
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h6" sx={{ marginRight: "10px" }}>
+          Request Status:{" "}
+        </Typography>
+        <Typography variant="body1">Pending</Typography>
+        {/* todo: change to real status */}
+      </div>
 
-            <div style={{ display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
-                <Typography variant="h6" sx={{ marginRight: "10px" }}>Request Status: </Typography>
-                <Typography variant="body1">Pending</Typography>
-                {/* todo: change to real status */}
-            </div>
+      <Divider sx={{ marginTop: "10px", marginBottom: "5px" }} />
+      <Typography component={"span"} variant="h6">
+        Chosen dates:
+      </Typography>
 
-            <Divider sx={{ marginTop: "10px", marginBottom: "5px" }} />
-            <Typography component={'span'} variant="h6">
-                Chosen dates:
-            </Typography>
+      <Divider sx={{ marginTop: "10px", marginBottom: "5px" }} />
+      <Row
+        tableData={[
+          { key: "Start Date:", value: selectedStartDate.toDateString() },
+          { key: "End Date:", value: selectedEndDate.toDateString() },
+        ]}
+      />
+      <Divider sx={{ marginTop: "10px", marginBottom: "10px" }} />
 
-            <Divider sx={{ marginTop: "10px", marginBottom: "5px" }} />
-            <Row
-                tableData={[
-                    { key: "Start Date:", value: selectedStartDate.toDateString() },
-                    { key: "End Date:", value: selectedEndDate.toDateString() },
-                ]}
-            />
-            <Divider sx={{ marginTop: "10px", marginBottom: "10px" }} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <DateRangePicker
+          startDate={selectedStartDate}
+          endDate={selectedEndDate}
+          onChange={() => {}}
+          datesToExclude={excludedDates}
+        />
+      </div>
 
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-                <DateRangePicker
-                    startDate={selectedStartDate}
-                    endDate={selectedEndDate}
-                    onChange={() => { }}
-                    datesToExclude={excludedDates}
-                />
-            </div>
-
-
-
-            <Button
-                variant="contained"
-                sx={{ mt: 1, mr: 1 }}
-                onClick={() => navigate(`/item/${itemId}`)} //todo: change back to all items?
-            >
-                Back
-            </Button>
-        </Container>
-    );
+      <Button
+        variant="contained"
+        sx={{ mt: 1, mr: 1 }}
+        onClick={() => navigate(`/item/${itemId}`)} //todo: change back to all items?
+      >
+        Back
+      </Button>
+    </Container>
+  );
 };
 
 export default BookingCompletedPage;
--- src/routes/requestToBookPage.tsx
+++ src/routes/requestToBookPage.tsx
@@ -1,256 +1,282 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Container } from "@mui/system";
-import {
-    Button,
-    Typography,
-    Divider,
-    Card,
-    CardMedia,
-} from "@mui/material";
-import HttpClient from "../api/HttpClient";
+import { Button, Typography, Divider, Card, CardMedia } from "@mui/material";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { IFullItemDetailsNew } from "../types";
 import DateRangePicker from "../components/DateRangePicker/DateRangePicker";
 import { formatImagesOnRecieve } from "../utils/imagesUtils";
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import { checkExcludeDatesArrayContainsDate, getFormattedDate } from "../utils/calendarUtils";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import {
+  checkExcludeDatesArrayContainsDate,
+  getFormattedDate,
+} from "../utils/calendarUtils";
+import { getItem } from "../api/ItemService";
 
 type IFullItemDetailsParams = {
-    itemId: string;
+  itemId: string;
 };
 
 type Props = {};
 
 interface IRowData {
-    key: string;
-    value: string;
+  key: string;
+  value: string;
 }
 
 interface ITableData {
-    tableData: IRowData[];
+  tableData: IRowData[];
 }
 
 const Row = ({ tableData }: ITableData) => {
-    return (
-        <div>
-            {tableData.map((row, i) => (
-                <div key={i}>
-                    <div style={{ display: "flex", flexDirection: "row" }}>
-                        <Typography
-                            variant="body1"
-                            sx={{ flexBasis: "50%", color: "darkgray" }}
-                        >
-                            {row.key}
-                        </Typography>
-                        <Typography variant="body1" sx={{ flexBasis: "50%" }}>
-                            {row.value}
-                        </Typography>
-                    </div>
-                    {i < tableData.length - 1 && <Divider sx={{ margin: "5px" }} />}
-                </div>
-            ))}
+  return (
+    <div>
+      {tableData.map((row, i) => (
+        <div key={i}>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <Typography
+              variant="body1"
+              sx={{ flexBasis: "50%", color: "darkgray" }}
+            >
+              {row.key}
+            </Typography>
+            <Typography variant="body1" sx={{ flexBasis: "50%" }}>
+              {row.value}
+            </Typography>
+          </div>
+          {i < tableData.length - 1 && <Divider sx={{ margin: "5px" }} />}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 const RequestToBookPage = (props: Props) => {
-    const location = useLocation();
-    const { selectedStartDate, selectedEndDate, excludedDates, onDateChange } = location.state;
+  const location = useLocation();
+  const {
+    selectedStartDate,
+    selectedEndDate,
+    excludedDates,
+    onDateChange,
+  } = location.state;
 
-    const [requestStartDate, setRequestStartDate] = useState<Date>(selectedStartDate);
-    const [requestEndDate, setRequestEndDate] = useState<Date>(selectedEndDate);
+  const [requestStartDate, setRequestStartDate] = useState<Date>(
+    selectedStartDate
+  );
+  const [requestEndDate, setRequestEndDate] = useState<Date>(selectedEndDate);
 
-    const [calendarStartDate, setCalendarStartDate] = useState<Date>(selectedStartDate);
-    const [calendarEndDate, setCalendarEndDate] = useState<Date>(selectedEndDate);
+  const [calendarStartDate, setCalendarStartDate] = useState<Date>(
+    selectedStartDate
+  );
+  const [calendarEndDate, setCalendarEndDate] = useState<Date>(selectedEndDate);
 
-    const [itemDetails, setItemDetails] = useState<IFullItemDetailsNew>({
-        categories: [],
-        condition: "",
-        itemId: "",
-        title: "",
-        images: [],
-        description: "",
-        excludedDates: [],
-    });
+  const [itemDetails, setItemDetails] = useState<IFullItemDetailsNew>({
+    categories: [],
+    condition: "",
+    itemId: "",
+    title: "",
+    images: [],
+    description: "",
+    excludedDates: [],
+  });
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    let { itemId } = useParams<IFullItemDetailsParams>();
+  let { itemId } = useParams<IFullItemDetailsParams>();
 
-    const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const handleChange = () => {
-        setRequestStartDate(calendarStartDate);
-        setRequestEndDate(calendarEndDate);
-        setOpen(false);
-    };
+  const handleChange = () => {
+    setRequestStartDate(calendarStartDate);
+    setRequestEndDate(calendarEndDate);
+    setOpen(false);
+  };
 
-    const [serverRequestError, setServerRequestError] = useState<any>();
+  const [serverRequestError, setServerRequestError] = useState<any>();
 
-    useEffect(() => {
-        const getFullDetails = async () => {
-            let fullDetails: IFullItemDetailsNew;
-            try {
-                fullDetails = await HttpClient.get(`items/${itemId}`);
-                setItemDetails(fullDetails);
-            }
-            catch (err) {
-                console.log("Error while loading item");
-                setServerRequestError(err);
-                //todo:show error
-            }
+  useEffect(() => {
+    const getFullDetails = async () => {
+      let fullDetails: IFullItemDetailsNew;
+      if (itemId) {
+        try {
+          fullDetails = (await getItem(itemId)) as IFullItemDetailsNew;
+          setItemDetails(fullDetails);
+        } catch (err) {
+          console.log("Error while loading item");
+          setServerRequestError(err);
+          //todo:show error
         }
-        getFullDetails();
-    }, []);
-
-    const [selectedDatesError, setSelectedDatesError] = useState<string>("");
-    const [isValidDates, setIsValidDates] = useState<boolean>();
-
-    //todo: add the invalid dates flow
-    const handleChangeDates = (dates: Date[]) => {
-        const [selectedStartDate, selectedEndDate] = dates;
-        setCalendarStartDate(selectedStartDate);
-        setCalendarEndDate(selectedEndDate);
-        setSelectedDatesError("");
-        let loop: Date = new Date(selectedStartDate);
-        if (selectedStartDate && selectedEndDate) {
-            while (loop <= selectedEndDate) {
-                if (
-                    (itemDetails.excludedDates !== undefined) && checkExcludeDatesArrayContainsDate(loop, itemDetails.excludedDates)
-                ) {
-                    setSelectedDatesError(
-                        "The date " +
-                        getFormattedDate(loop) +
-                        " is not available, please choose different dates."
-                    );
-                    setIsValidDates(false);
-                    break;
-                } else {
-                    setCalendarStartDate(selectedStartDate);
-                    setCalendarEndDate(selectedEndDate);
-                    setSelectedDatesError("");
-                    setIsValidDates(true);
-                }
-
-                let newDate = loop.setDate(loop.getDate() + 1);
-                loop = new Date(newDate);
-            }
-        }
+      }
     };
+    getFullDetails();
+  }, []);
 
+  const [selectedDatesError, setSelectedDatesError] = useState<string>("");
+  const [isValidDates, setIsValidDates] = useState<boolean>();
 
+  //todo: add the invalid dates flow
+  const handleChangeDates = (dates: Date[]) => {
+    const [selectedStartDate, selectedEndDate] = dates;
+    setCalendarStartDate(selectedStartDate);
+    setCalendarEndDate(selectedEndDate);
+    setSelectedDatesError("");
+    let loop: Date = new Date(selectedStartDate);
+    if (selectedStartDate && selectedEndDate) {
+      while (loop <= selectedEndDate) {
+        if (
+          itemDetails.excludedDates !== undefined &&
+          checkExcludeDatesArrayContainsDate(loop, itemDetails.excludedDates)
+        ) {
+          setSelectedDatesError(
+            "The date " +
+              getFormattedDate(loop) +
+              " is not available, please choose different dates."
+          );
+          setIsValidDates(false);
+          break;
+        } else {
+          setCalendarStartDate(selectedStartDate);
+          setCalendarEndDate(selectedEndDate);
+          setSelectedDatesError("");
+          setIsValidDates(true);
+        }
 
-    return (
-        <Container>
-            <Typography component={'span'} variant="h3">Request To Book</Typography>
+        let newDate = loop.setDate(loop.getDate() + 1);
+        loop = new Date(newDate);
+      }
+    }
+  };
 
-            <div style={{ display: "flex", alignItems: "flex-start", flexDirection: "row" }}>
-                <Card sx={{ marginBottom: "10px", width: "230px", marginRight: "10px" }}>
-                    {itemDetails.images && (
-                        <CardMedia
-                            component="img"
-                            style={{ height: "230px", width: "230px" }}
-                            image={formatImagesOnRecieve(itemDetails.images)[0]}
-                        >
-                        </CardMedia>
-                    )}
-                </Card>
+  return (
+    <Container>
+      <Typography component={"span"} variant="h3">
+        Request To Book
+      </Typography>
 
-                <div style={{ display: "flex", flexDirection: "column" }}>
-                    <Typography variant="h5">{itemDetails.title}</Typography>
-                    <Typography variant="body1">{itemDetails.description}</Typography>
-                </div>
-            </div>
-            <Typography component={'span'} variant="h6">Chosen Dates:</Typography>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "flex-start",
+          flexDirection: "row",
+        }}
+      >
+        <Card
+          sx={{ marginBottom: "10px", width: "230px", marginRight: "10px" }}
+        >
+          {itemDetails.images && (
+            <CardMedia
+              component="img"
+              style={{ height: "230px", width: "230px" }}
+              image={formatImagesOnRecieve(itemDetails.images)[0]}
+            ></CardMedia>
+          )}
+        </Card>
 
-            <Divider sx={{ marginTop: "10px", marginBottom: "5px" }} />
-            <Row
-                tableData={[
-                    { key: "Start Date:", value: requestStartDate.toDateString() },
-                    { key: "End Date:", value: requestEndDate.toDateString() },
-                ]}
-            />
-            <Divider sx={{ marginTop: "10px", marginBottom: "10px" }} />
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <Typography variant="h5">{itemDetails.title}</Typography>
+          <Typography variant="body1">{itemDetails.description}</Typography>
+        </div>
+      </div>
+      <Typography component={"span"} variant="h6">
+        Chosen Dates:
+      </Typography>
 
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-                <DateRangePicker
-                    startDate={requestStartDate}
-                    endDate={requestEndDate}
-                    onChange={() => { }}
-                    datesToExclude={excludedDates}
-                />
+      <Divider sx={{ marginTop: "10px", marginBottom: "5px" }} />
+      <Row
+        tableData={[
+          { key: "Start Date:", value: requestStartDate.toDateString() },
+          { key: "End Date:", value: requestEndDate.toDateString() },
+        ]}
+      />
+      <Divider sx={{ marginTop: "10px", marginBottom: "10px" }} />
 
-                <div>
-                    <Button variant="contained" sx={{ mt: 1, mr: 1 }} onClick={handleClickOpen}>
-                        Edit Dates
-                    </Button>
-                    <Dialog open={open} onClose={handleClose}>
-                        <DialogTitle>Edit Dates</DialogTitle>
-                        <DialogContent>
-                            <DialogContentText>
-                                Please choose your desired dates:
-                            </DialogContentText>
-                            <DateRangePicker
-                                startDate={calendarStartDate}
-                                endDate={calendarEndDate}
-                                onChange={handleChangeDates}
-                                datesToExclude={excludedDates}
-                            />
-                            {calendarStartDate && (
-                                <p>start date: {calendarStartDate.toDateString()}</p> //todo: change <p>
-                            )}
-                            {calendarEndDate && (
-                                <p>end date: {calendarEndDate.toDateString()}</p>
-                            )}
-                        </DialogContent>
-                        <DialogActions>
-                            <Button onClick={handleClose}>Cancel</Button>
-                            <Button onClick={handleChange}>change</Button>
-                        </DialogActions>
-                    </Dialog>
-                </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <DateRangePicker
+          startDate={requestStartDate}
+          endDate={requestEndDate}
+          onChange={() => {}}
+          datesToExclude={excludedDates}
+        />
 
-            </div>
+        <div>
+          <Button
+            variant="contained"
+            sx={{ mt: 1, mr: 1 }}
+            onClick={handleClickOpen}
+          >
+            Edit Dates
+          </Button>
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Edit Dates</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Please choose your desired dates:
+              </DialogContentText>
+              <DateRangePicker
+                startDate={calendarStartDate}
+                endDate={calendarEndDate}
+                onChange={handleChangeDates}
+                datesToExclude={excludedDates}
+              />
+              {calendarStartDate && (
+                <p>start date: {calendarStartDate.toDateString()}</p> //todo: change <p>
+              )}
+              {calendarEndDate && (
+                <p>end date: {calendarEndDate.toDateString()}</p>
+              )}
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={handleChange}>change</Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+      </div>
 
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <Button
-                    variant="contained"
-                    sx={{ mt: 1, mr: 1 }}
-                    onClick={() => navigate(`/item/${itemId}`)}
-                >
-                    Back
-                </Button>
-                <Button
-                    variant="contained"
-                    sx={{ mt: 1, mr: 1 }}
-                    onClick={() => navigate(`/bookingCompletedPage/${itemId}`,
-                        {
-                            state:
-                            {
-                                selectedStartDate: requestStartDate,
-                                selectedEndDate: requestEndDate,
-                                excludedDates
-                            }
-                        })}
-                >
-                    Confirm
-                </Button>
-            </div>
-
-        </Container>
-    );
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Button
+          variant="contained"
+          sx={{ mt: 1, mr: 1 }}
+          onClick={() => navigate(`/item/${itemId}`)}
+        >
+          Back
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ mt: 1, mr: 1 }}
+          onClick={() =>
+            navigate(`/bookingCompletedPage/${itemId}`, {
+              state: {
+                selectedStartDate: requestStartDate,
+                selectedEndDate: requestEndDate,
+                excludedDates,
+              },
+            })
+          }
+        >
+          Confirm
+        </Button>
+      </div>
+    </Container>
+  );
 };
 
 export default RequestToBookPage;
