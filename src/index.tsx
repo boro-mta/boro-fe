fa616c05b870178f9e51662ce519c930e605eab0
AlonDoron <alon1212112@gmail.com>
2023-08-18 16:59:11 +0300

    Vite update

--- src/index.tsx
+++ src/index.tsx
@@ -18,11 +18,6 @@ import ErrorPage from "./routes/errorPage";
 import FacebookLoginPage from "./routes/facebookLoginPage";
 import "react-datepicker/dist/react-datepicker.css";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import AddItemPage from "./routes/addItemPage";
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import svgrPlugin from "vite-plugin-svgr";
+import dns from "dns";
+
+dns.setDefaultResultOrder("verbatim");
+
+export default defineConfig({
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  server: {
+    port: 3000,
+    open: true,
+    host: "localhost",
+  },
+});
